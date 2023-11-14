9c96ed8fa576e6f3c5bba0c73f9357b7d3925d30
Jesse Shelley <realjesseshelley@gmail.com>
2023-11-13 21:07:49 -0700

    Added configs

--- lingather.py
+++ lingather.py
@@ -2,6 +2,21 @@
 import subprocess
 import os
 import grp
+import pathlib
+
+
+def print_config(name: str, path: str) -> None:
+    pathname = pathlib.Path(path)
+    filename = pathlib.Path(path).name
+    if not pathlib.Path.exists(pathname):
+        print(f"{name} - {pathname}")
+        print(f"{path} not found.")
+    else:
+        with open(path, 'r') as config:
+            print(f"{name} - ({pathname})")
+            for line in config:
+                print(f"{filename}>>> {line.strip()}")
+            print('\n')
 
 
 def distribution(): return subprocess.run(["cat /etc/*issue"], shell=True, capture_output=True)
@@ -28,8 +43,14 @@ def interfaces_ifconfig():
 
 
 def routes(): return subprocess.run(["netstat -rn"], shell=True, capture_output=True)
+
+
 def netstat(): return subprocess.run(["netstat -anpt"], shell=True, capture_output=True)
+
+
 def processes(): return subprocess.run(["ps aux"], shell=True, capture_output=True)
+
+
 def environment(): return subprocess.run(["env"], shell=True, capture_output=True)
 
 
@@ -62,3 +83,9 @@ print(f"""RUNNING PROCESSES
 print(f"""ENVIRONMENT
 {environment().stdout.decode().strip()}
 """)
+
+# APACHE CONFIGURATION
+print_config("APACHE2 CONFIGURATION", "/etc/apache2/apache2.conf")
+print_config("APACHE2 PORTS CONFIGURATION", "/etc/apache2/ports.conf")
+print_config("NGINX CONFIGURATION", "/etc/nginx/nginx.conf")
+print_config("SNORT CONFIGURATION", "/etc/snort/snort.conf")
